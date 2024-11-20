91f3db3144f143a2dc249c833160b93d55d3b03c
Рябов Сергей Дмитриевич <ryabov-sd@ranepa.ru>
2024-11-20 15:28:44 +0300

    homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,12 @@
 import { Button } from "@/components/ui/button"
 import { FiDownload } from "react-icons/fi"
 
-export default function Home () {
+// components
+import Social from "@/components/Social";
+import Photo from "@/components/Photo";
+import Stats from "@/components/Stats";
+
+export default function Home() {
   return (
     <section className="h-full">
       <div className="container mx-auto h-full">
@@ -9,10 +14,10 @@ export default function Home () {
         xl:pt-8 xl:pb-24">
 
           {/* text */}
-          <div className="text-center xl:text-left">
+          <div className="text-center xl:text-left order-2 xl:order-none">
             <span className="text-xl">Веб-разработчик</span>
             <h1 className="h1 mb-6">
-              Привет Я <br /> <span className="text-accent">Ryabov Sergey</span>
+              Привет Я <br /> <span className="text-accent">Рябов Сергей</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
               Я преуспеваю в создании элегантных цифровых решений
@@ -22,20 +27,29 @@ export default function Home () {
             {/* btn and socials */}
             <div className="flex flex-col xl:flex-row items-center gap-8">
               <Button
-                vaiant="outline"
+                variant="outline"
                 size="lg"
                 className="uppercase flex items-center gap-2">
                 <span>Download CV</span>
                 <FiDownload className="text-xl" />
               </Button>
-              <div>socials</div>
+              <div className="mb-8 xl:mb-0">
+                <Social
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-accent rounded-full flex
+                  justify-center items-center text-accent text-base hover:bg-accent
+                  hover:text-primary hover:transition-all duration-500"/>
+              </div>
             </div>
           </div>
 
           {/* photo */}
-          <div>photo</div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
+          </div>
         </div>
       </div>
+      <Stats />
     </section>
   );
 };
